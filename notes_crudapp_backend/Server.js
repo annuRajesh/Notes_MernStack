adce72061b8c851bfffd737f1b33d714521abb4d
rollinggir <joannetsengannu@gmail.com>
2024-08-13 23:04:48 +0530

    account creation endpoint is created.

--- notes_crudapp_backend/Server.js
+++ notes_crudapp_backend/Server.js
@@ -16,7 +16,8 @@ const MongodbUrl="mongodb://127.0.0.1:27017/Notes_App"
 mongoose.connect(MongodbUrl).then(()=>{
     console.log("connected to database")
 })
-const user=mongoose.Schema({
+//schema defined for users collection
+const User=mongoose.Schema({
     username:{
         type:String,
         required:true
@@ -30,5 +31,26 @@ const user=mongoose.Schema({
         type:String,
         required:true
 
+    }
+})
+//model for the users created
+const user=mongoose.model("users",User)
+app.post("/api/createAccount",async(req,res)=>{
+    try{
+
+        const {username,email,password}=req.body
+        
+        
+        const newuser=new user({username,email,password})
+        await newuser.save()
+        console.log("Account created successfully")
+        res.status(201).json({message:"Account created successfully"})
+    }
+    catch(error){
+        if(error.code===11000){
+            res.status(400).json({message:"Email already exist"})
+        }
+        console.log("Error in creating account:",error)
+       
     }
 })
--- notes_crudapp_frontend/notes-crudapp/src/components/SignUp.jsx
+++ notes_crudapp_frontend/notes-crudapp/src/components/SignUp.jsx
@@ -1,55 +1,140 @@
-import { useNavigate } from "react-router-dom"
-import NavBar from "./NavBar"
+import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import { useState } from "react";
+import validateEmail from "./Validate";
+import axios from "axios";
 
-const SignUp=()=>{
-    const navigate=useNavigate()
-    const handleNavigation=()=>{
-     navigate('/login')
 
+const SignUp = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (!email || !name || !password || !rePassword) {
+        setError("all fields are required to fill");
+        return;
+      }
+      if (!validateEmail(email)) {
+        setError("Enter valid email address");
+        return;
+      }
+      if (password.length < 8) {
+        setError("password must have atleast 8 characters");
+        return;
+      }
+      if (!/^[A-Z]/.test(password)) {
+        setError("password must begin with a capital letter");
+        return;
+      }
+      if (password != rePassword) {
+        setError("password retyped not a match");
+        return;
+      }
+
+      const response = await axios.post(
+        "http://localhost:5000/api/createAccount",
+        { username:name, email:email,password: password }
+        
+      );
+     
+      console.log(response.data.message)
+      setError("");
+      if (response.status === 201) {
+        alert(response.data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log("error happened:", error);
     }
-    return(
-        <>
-        <NavBar/>
-        <div className="container flex flex-col justify-center text-center mx-auto my-10 border-2 p-3 max-w-screen-sm rounded-lg shadow-xl border-RoyalBlue">
-            <div className="text-2xl p-2">
-
-            <h1>Registration</h1>
-            </div>
-            <form action="" className="flex flex-col justify-center">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <input type="text" placeholder="name" className="bg-RoyalBlue outline-none m-1 px-4 py-2"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="text" placeholder="email" className="bg-RoyalBlue px-4 py-2 outline-none"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-
-                            <input type="password" placeholder="password" className="bg-RoyalBlue m-1 px-4 py-2 outline-none"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="password" placeholder="rewrite the password" className="bg-RoyalBlue px-4 py-2 outline-none" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <button className="bg-secondaryYellow m-4 text-black px-24 py-2">Sign Up</button>
-                                <p className="text-sm">Already registered?<span className="text-secondaryYellow underline cursor-pointer" onClick={handleNavigation}>Login</span></p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
+  };
+  const handleNavigation = () => {
+    navigate("/login");
+  };
+  return (
+    <>
+      <NavBar />
+      <div className="container flex flex-col justify-center text-center mx-auto my-10 border-2 p-3 max-w-screen-sm rounded-lg shadow-xl border-RoyalBlue">
+        <div className="text-2xl p-2">
+          <h1>Registration</h1>
         </div>
-        </>
-    )
-}
-export default SignUp
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center"
+        >
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    placeholder="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="bg-RoyalBlue outline-none m-1 px-4 py-2"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="bg-RoyalBlue px-4 py-2 outline-none"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="bg-RoyalBlue m-1 px-4 py-2 outline-none"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="password"
+                    placeholder="rewrite the password"
+                    value={rePassword}
+                    onChange={(e) => setRePassword(e.target.value)}
+                    className="bg-RoyalBlue px-4 py-2 outline-none"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="bg-secondaryYellow m-4 text-black px-24 py-2">
+                    Sign Up
+                  </button>
+                  {error && <p className="text-sm text-red-400">{error}</p>}
+                  <p className="text-sm">
+                    Already registered?
+                    <span
+                      className="text-secondaryYellow underline cursor-pointer"
+                      onClick={handleNavigation}
+                    >
+                      Login
+                    </span>
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
+    </>
+  );
+};
+export default SignUp;
